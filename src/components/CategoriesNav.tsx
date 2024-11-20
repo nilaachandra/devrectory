e7df9a3d32418d343ce37209023a89dba7365e90
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-11-20 23:50:15 +0530

    minor changes

--- src/components/CategoriesNav.tsx
+++ src/components/CategoriesNav.tsx
@@ -30,8 +30,8 @@ export default function Shelf() {
               }}
               className={`rounded-md px-3 py-1 text-sm font-medium shrink-0 ${
                 activeIndex === index
-                  ? "bg-zinc-200 text-zinc-900 dark:bg-zinc-800 dark:text-zinc-100"
-                  : "bg-zinc-100 text-zinc-600 dark:bg-zinc-700 dark:text-zinc-400"
+                  ? "bg-zinc-200 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100"
+                  : "bg-zinc-100 text-zinc-600 dark:bg-zinc-800 dark:text-zinc-400"
               }`}
             >
               <span className="text-xl lg:text-base">{item.icon}</span>{" "}
